b73f2cc69812530d14d4a9b19239f6a2ee08af4a
Komal-Jayswal <Komal.Jayswal@maersk.com>
2024-03-26 19:42:00 +0530

    reformate

--- src/main/java/net/learning/Temporal/TemporalWithSpringbootApplication.java
+++ src/main/java/net/learning/Temporal/TemporalWithSpringbootApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TemporalWithSpringbootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TemporalWithSpringbootApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TemporalWithSpringbootApplication.class, args);
+    }
 
 }
